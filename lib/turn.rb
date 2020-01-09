9634516b99deadf3c32848ca3b29d6ba5991cfbc
Daniel Osment <osment.tsundu@gmail.com>
2020-01-09 07:23:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board,index)
-  && index.between?(0,8) !position_taken?(board, index)
+ index.between?(0,8)   &&!position_taken?(board, index)
 end
 
 def position_taken?(board, index)
